9d9ce1ce8de11bf52e59d0a3805196590bb536a3
your_name <your_email@gmail.com>
2018-06-14 15:53:51 +0800

    练习git

---
+++ src/main/java/com/cheduoduo/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.cheduoduo.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+
+
+
+
+   @GetMapping("/hello")
+    String index(){
+        return "Hello , world";
+}
+}
